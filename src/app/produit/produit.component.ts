f63edf361371ca83656dca0da94c85a661dcf1ee
Khaled BOUSSABA <khaledboussaba@gmail.com>
2020-03-23 16:55:41 +0100

    data collected from mysql database OK

--- src/app/produit/produit.component.ts
+++ src/app/produit/produit.component.ts
@@ -69,7 +69,7 @@ export class ProduitComponent implements OnInit {
     }
 
     deleteProduit() {
-        this.produitService.deleteProduit(this.selectedProduit.ref).subscribe(
+        this.produitService.deleteProduit(this.selectedProduit.id).subscribe(
             res => {
                 this.selectedProduit = new Produit();
                 this.loadProduits();
