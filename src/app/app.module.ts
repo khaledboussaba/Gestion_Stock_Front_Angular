0f9a6f88e7dfae5cabb527bda2d2e13c73217568
Khaled BOUSSABA <khaledboussaba@gmail.com>
2020-03-23 12:07:00 +0100

    add of routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ProduitComponent } from './produit/produit.component';
@@ -9,6 +8,9 @@ import { ProduitMockService } from './produit/produit.mock.service';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContentComponent } from './content/content.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { AppRoutingModule } from './app.routing.module';
 
 
 @NgModule({
@@ -17,7 +19,8 @@ import { ContentComponent } from './content/content.component';
     ProduitComponent,
     NavbarComponent,
     SidebarComponent,
-    ContentComponent
+    ContentComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
